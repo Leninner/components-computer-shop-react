ed653b5a6a4621ce18730b1006da4b4776af66ba
leninner <mazabandalenin180@gmail.com>
2021-11-01 14:04:57 -0500

    Update

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='mx-auto text-center container h-screen bg-gradient-to-r from-green-400 to-blue-500'>
+      <h1 className='text-5xl p-5 shadow-2xl'>Hola mundito</h1>
+    </div>
+  );
+};
+
+export { App };
