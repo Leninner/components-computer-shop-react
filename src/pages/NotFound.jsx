a2c01811489b749c139d591c9d3891dc729915e5
leninner <mazabandalenin180@gmail.com>
2021-11-05 15:45:05 -0500

    Update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const NotFound = () => {
-  return <div>Página no encontrada</div>;
+  return (
+    <section className='w-screen h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500'>
+      <div className='flex flex-col items-center justify-center h-screen gap-3 text-white'>
+        <h1 className='mb-12 font-bold text-9xl animate-ping'>404</h1>
+        <h3 className='text-xl animate-pulse'>Está página no pudo ser encontrada</h3>
+        <button className='notFound'>Vuelve al inicio</button>
+      </div>
+    </section>
+  );
 };
 
 // TODO: Hacer una página de 404
