734577d87f02b9bb605b0f43fdb788dbca1ecd52
leninner <mazabandalenin180@gmail.com>
2021-11-08 10:36:22 -0500

    Añado estilos a las página 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import '../styles/NotFound.scss';
 const NotFound = () => {
   return (
     <section className='page404'>
-      <div className=''>
+      <div className='content info'>
         <h1 className=''>404</h1>
         <h3 className=''>Esta página no pudo ser encontrada</h3>
         <button className='notFound'>Vuelve al inicio</button>
