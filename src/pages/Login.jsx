f4f85c04d85aeb522f19b5bc4297a8391ddc42d7
leninner <mazabandalenin180@gmail.com>
2021-11-09 16:26:52 -0500

    Add useRef hook to the login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '@styles/Login.scss';
 import logo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const data = { username: formData.get('email'), password: formData.get('password') };
+    console.log(data);
+  };
+
   return (
-    <div className='login'>
-      <div className='form-container'>
+    <div className='Login'>
+      <div className='Login-container'>
         <img src={logo} alt='logo' className='logo' />
-
-        <h1 className='title'>Create a new password</h1>
-        <p className='subtitle'>Enter a new passwrd for yue account</p>
-
-        <form action='/' className='form'>
+        <form action='/' className='form' ref={form}>
+          <label htmlFor='email' className='label'>
+            Email address
+          </label>
+          <input type='text' name='email' placeholder='platzi@example.cm' className='input input-email' />
           <label htmlFor='password' className='label'>
             Password
           </label>
-          <input type='password' id='password' placeholder='*********' className='input input-password' />
-
-          <label htmlFor='new-password' className='label'>
-            Password
-          </label>
-          <input type='password' id='new-password' placeholder='*********' className='input input-password' />
-
-          <input type='submit' value='Confirm' className='primary-button login-button' />
+          <input type='password' name='password' placeholder='*********' className='input input-password' />
+          <button onClick={(e) => handleSubmit(e)} className='primary-button login-button'>
+            Log in
+          </button>
+          <a href='/'>Forgot my password</a>
         </form>
+        <button className='secondary-button signup-button'>Sign up</button>
       </div>
     </div>
   );
