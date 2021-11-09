4e272d054c82a3cf06f7423e5082db3760cb27cb
leninner <mazabandalenin180@gmail.com>
2021-11-09 14:48:15 -0500

    Add useState Hook

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+
+  const handleCart = () => {
+    setCart([]);
+  };
+
   return (
     <div className='ProductItem'>
       <img
@@ -14,7 +20,7 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleCart}>
           <img src={addToCart} alt='' />
         </figure>
       </div>
