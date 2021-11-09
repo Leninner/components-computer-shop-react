4accd2ef8dd6882e754f3925ee87f142b22618df
leninner <mazabandalenin180@gmail.com>
2021-11-09 15:05:44 -0500

    Add styles and handleToggle for Users menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,18 @@ import '@styles/Header.scss';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import { Menu } from '@components/Menu';
 
 const Header = () => {
+  const [toggle, setToggle] = React.useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav>
-      <img src={menu} alt='menu' className='menu' />
+      <img src={menu} alt='menu' className='menu' onClick={handleToggle} />
 
       <div className='navbar-left'>
         <img src={logo} alt='logo' className='nav-logo' />
@@ -36,13 +43,16 @@ const Header = () => {
 
       <div className='navbar-right'>
         <ul>
-          <li className='navbar-email'>platzi@example.com</li>
+          <li className='navbar-email' onClick={handleToggle}>
+            platzi@example.com
+          </li>
           <li className='navbar-shopping-cart'>
             <img src={shoppingCart} alt='shopping cart' />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
     </nav>
   );
 };
