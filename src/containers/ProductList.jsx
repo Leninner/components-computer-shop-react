fb3d508617b2007c71991304511b07ef018a4185
leninner <mazabandalenin180@gmail.com>
2021-11-10 17:59:13 -0500

    [FIX] 'menu and modal disappears  problem'

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ProductItem } from '@components/ProductItem';
 import '@styles/ProductList.scss';
 import { useGetProducts } from '@hooks/useGetProducts';
