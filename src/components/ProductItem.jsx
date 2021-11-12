92bbb4b934f04123b0c3e4ed21f3579a4db15906
leninner <mazabandalenin180@gmail.com>
2021-11-12 13:38:29 -0500

    Try

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,25 +1,28 @@
 import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import addCart from '@icons/bt_add_to_cart.svg';
+import addedToCart from '@icons/bt_added_to_cart.svg';
 import { AppContext } from '@context/AppContext';
 
 const ProductItem = ({ product }) => {
   const { addToCart } = useContext(AppContext);
+  const [addedCart, setAddedCart] = React.useState(false);
 
   const handleClick = (item) => {
     addToCart(item);
+    setAddedCart(!addedCart);
   };
 
   return (
     <div className='ProductItem'>
-      <img src={product.images[2]} alt={product.title} />
+      <img src={product.images[0]} alt={product.title} />
       <div className='product-info'>
         <div>
           <p>${product.price},00</p>
           <p>{product.title}</p>
         </div>
         <figure onClick={() => handleClick(product)}>
-          <img src={addCart} alt='' />
+          <img src={addedCart ? addedToCart : addCart} alt='' />
         </figure>
       </div>
     </div>
